fa64b4317c58833ebbcad46f42db37ad7d056348
Burak Arslan <49092119+arslan38@users.noreply.github.com>
2026-01-09 21:11:43 +0300

    fix: update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'https://agit-stuff.github.io',
+  site: 'https://agit.github.io',
   integrations: [
     starlight({
       title: 'AGIT',
